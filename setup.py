511652f06fb6114dfcf3e294685ab570852fda69
Ben Gamari <bgamari.foss@gmail.com>
2013-07-30 14:07:00 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='notmuch-indicator',
+      version='1.0',
+      description="Notmuch indicator for Ubuntu's messaging menu",
+      author='Ben Gamari',
+      author_email='bgamari.foss@gmail.com',
+      url='http://www.github.com/bgamari/notmuch-indicator',
+      scripts=['notmuch-indicator'],
+     )
